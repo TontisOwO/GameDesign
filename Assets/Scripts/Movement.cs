cf2d0f68ebac7245f29233a39a27194a81e81fc7
Anton Vahlenberg <anton.vahlenberg@learnet.se>
2024-12-05 09:39:55 +0100

    yeet

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -10,6 +10,8 @@ public class Movement : MonoBehaviour
 {
     Rigidbody2D myRigidbody;
     [SerializeField] float movementSpeed;
+    float movementSpeedLeft;
+    float movementSpeedRight;
     [SerializeField] float jumpSpeed;
     float jumpFactor;
     float jumpTime;
@@ -25,19 +27,39 @@ public class Movement : MonoBehaviour
 
         if (Input.GetKey(KeyCode.A))
         {
-            movementVector.x -= movementSpeed * Time.deltaTime;
+            if (movementSpeedLeft < movementSpeed)
+            {
+                movementSpeedLeft += Time.deltaTime * movementSpeed * 5;
+            }
+            movementVector.x -= movementSpeedLeft * Time.deltaTime;
+        }
+        if (!Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            movementSpeedLeft = movementSpeed * 0.25f;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            movementVector.x += movementSpeed * Time.deltaTime;
+            if (movementSpeedRight < movementSpeed)
+            {
+                movementSpeedRight += Time.deltaTime * movementSpeed * 5;
+            }
+            movementVector.x += movementSpeedRight * Time.deltaTime;
+        }
+        if (!Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
+        {
+            movementSpeedRight = movementSpeed * 0.25f;
+
         }
 
         if (Input.GetKey(KeyCode.Space) && jumpState == CharacterState.Grounded)
         {
+            if (myRigidbody.linearVelocityY < 0)
+            {
+                myRigidbody.linearVelocityY = 0;
+            }
             myRigidbody.linearVelocityY += jumpSpeed;
             jumpState = CharacterState.Jumping;
-            Debug.Log("eh?");
         }
 
         if (jumpState == CharacterState.Jumping)
@@ -47,14 +69,16 @@ public class Movement : MonoBehaviour
 
         if (Input.GetKeyUp(KeyCode.Space))
         {
+            jumpState = CharacterState.Air;
+
             switch (jumpTime, jumpTime)
             {
                 case ( > 0, <= 0.1f):
-                    jumpFactor = 0.5f;
+                    jumpFactor = 0.7f;
                     break;
 
                 case ( > 0.1f, <= 0.25f):
-                    jumpFactor = 0.25f;
+                    jumpFactor = 0.3f;
                     break;
 
                 case ( > 0.25f, <= 100f):
@@ -63,7 +87,6 @@ public class Movement : MonoBehaviour
             }
             myRigidbody.linearVelocity -= new Vector2(0, jumpFactor * jumpSpeed);
             jumpTime = 0;
-            jumpState = CharacterState.Air;
         }
 
         transform.position = movementVector;
@@ -75,10 +98,16 @@ public class Movement : MonoBehaviour
     //        Land();
     //    }
     //}
-    public void Land()
+    public void Land(float groundSpeed)
     {
         jumpState = CharacterState.Grounded;
+        myRigidbody.linearVelocityY = groundSpeed;
         jumpTime = 0;
+    }
+    public void OnGround(float groundSpeed)
+    {
+        myRigidbody.linearVelocityY = groundSpeed;
+        jumpState = CharacterState.Grounded;
     }
     //void OnCollisionExit2D(Collision2D other)
     //{
---
+++ Assets/Scripts/PlatformScript.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour
+{
+    [SerializeField] bool isStatic;
+    [SerializeField] float waitTime = 1.0f;
+    [SerializeField] Vector2 destination;
+    Vector2 vectorValue0;
+    [SerializeField] float timeTaken;
+    Rigidbody2D myRidgidbody;
+    private void Awake()
+    {
+        vectorValue0 = Vector2.zero;
+        myRidgidbody = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        timeTaken += Time.deltaTime;
+        if (!isStatic)
+        {
+            switch (timeTaken)
+            {
+                case < 1f:
+                    myRidgidbody.linearVelocity = Vector2.Lerp(vectorValue0, destination, 1);
+                    break;
+                case < 2f:
+                    myRidgidbody.linearVelocity = vectorValue0;
+                    break;
+                case < 3f:
+                    myRidgidbody.linearVelocity = Vector2.Lerp(vectorValue0, -destination, 1);
+                    break;
+                case < 4f:
+                    myRidgidbody.linearVelocity = vectorValue0;
+                    break;
+                case < 5f:
+                    timeTaken = 0;
+                    break;
+            }
+        }
+    }
+}
